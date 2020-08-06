5535078342697227f975e4e82db71e48c7e776aa
Thanos Diacakis <thanos@diacakis.com>
2020-08-05 19:40:39 -0700

    Upgrading to Django 3.1

--- db_email_backend/models.py
+++ db_email_backend/models.py
@@ -2,10 +2,8 @@
 from __future__ import unicode_literals
 
 from django.db import models
-from django.utils.encoding import python_2_unicode_compatible
 
 
-@python_2_unicode_compatible
 class Email(models.Model):
     create_date = models.DateTimeField(auto_now_add=True)
     subject = models.TextField(blank=True)
@@ -21,7 +19,6 @@ class Email(models.Model):
         return '{} - {}, {}'.format(self.subject, self.to, self.create_date)
 
 
-@python_2_unicode_compatible
 class EmailAlternative(models.Model):
     email = models.ForeignKey(Email, on_delete=models.CASCADE, related_name='alternatives')
     content = models.TextField(blank=True)
@@ -31,7 +28,6 @@ class EmailAlternative(models.Model):
         return '{}: alternative {}'.format(self.email, self.mimetype)
 
 
-@python_2_unicode_compatible
 class EmailAttachment(models.Model):
     email = models.ForeignKey(Email, on_delete=models.CASCADE, related_name='attachments')
     filename = models.CharField(max_length=1000, blank=True)
